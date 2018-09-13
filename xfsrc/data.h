0e765e139e56b95a0791f9568a04cd49c4adfcc8
Oleg Grytsynevych <oleg.gritsinevich@gmail.com>
2018-09-13 09:06:36 -0700

    Reduced amount of source files to the bare minimum.

--- xfsrc/data.h
+++
@@ -1,8 +0,0 @@
-typedef UCP 	FCP;
-typedef void *			FVP;
-typedef FVP *			FVPP;
-
-extern void putA(FVP fvp, ARRAY *ptr);
-extern ARRAY *getA(FVP fvp);		// get(possibly nested) array from apl	
-
-extern void freeA(void);
